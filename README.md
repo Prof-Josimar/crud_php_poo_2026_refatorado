61305e0d252e80e57392a3e7f8d4d7effd09a589
profjosimar <josimar.ribeiro@prof.eteprc.faetec.rj.gov.br>
2026-09-23 19:45:36 -0300

    Commit em  23/09/2026 às 19:45:36,21

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-"# crud_php_poo_2026_refatorado 23/09/2026 19:43:09,95 " 
+# Projeto Aulas PHP 2026 Refatorado - 23/09/2026 19:45:35,96 
+ 
+ 
+ 
+## Informações do sistema 
+- Data e hora: 23/09/2026 19:45:35,96 
+- Usuário: admin 
+- Computador: FE16 
+- Diretório atual: C:\dev\php\crud_php_poo_2026_refatorado 
+- git version 2.55.0.windows.3 
+ 
+<img src="https://github.com/Prof-Josimar/crud_php_poo_2026/blob/main/public/imagens/logo.png" width="300"> 
+ 
+ M README.md
+ M up.cmd
+ 
+ 
+## Autor 
+Josimar Ribeiro 
+ 
+<img src="https://raw.githubusercontent.com/juanferreira-x/juanferreira-x/output/github-contribution-grid-snake.svg" width="600"> 
--- up.cmd
+++ up.cmd
@@ -1,7 +1,64 @@
-echo "# crud_php_poo_2026_refatorado %date% %time% " > README.md
-git init
+@echo off
+chcp 65001
+rem c:\xampp\mysql\bin\mysqldump -u root --databases --skip-comments aulapdo > .\src\Utils\backup_DB_Com_Dados.sql
+rem c:\xampp\mysql\bin\mysqldump -u root --no-data --databases --skip-comments   aulapdo > .\src\Utils\backup_DB_SemDados.sql
+
+set "project_name=Projeto Aulas PHP 2026 Refatorado - %date% %time%"
+set "author=Josimar Ribeiro"
+set "filename=README.md"
+set "date_time=%DATE% %TIME%"
+set "logo_url=https://github.com/Prof-Josimar/crud_php_poo_2026/blob/main/public/imagens/logo.png"
+
+REM === Cria o README.md ===
+echo # %project_name% > %filename%
+echo. >> %filename%
+
+
+echo. >> %filename%
+echo. >> %filename%
+
+
+
+echo ## Informações do sistema >> %filename%
+echo - Data e hora: %date_time% >> %filename%
+echo - Usuário: %USERNAME% >> %filename%
+echo - Computador: %COMPUTERNAME% >> %filename%
+echo - Diretório atual: %CD% >> %filename%
+for /f "tokens=* delims=" %%g in ('git --version') do echo - %%g >> %filename%
+echo. >> %filename%
+
+echo ^<img src="%logo_url%" width="300"^> >> %filename%
+echo. >> %filename%
+
+REM === Bloco para git status ===
+git status --porcelain >> %filename%
+
+REM === Bloco de Download ===
+echo. >> %filename%
+
+
+
+
+echo. >> %filename%
+
+echo ## Autor >> %filename%
+echo %author% >> %filename%
+echo. >> %filename%
+
+set "logo_url=https://raw.githubusercontent.com/juanferreira-x/juanferreira-x/output/github-contribution-grid-snake.svg"
+
+
+echo ^<img src="%logo_url%" width="600"^> >> %filename%
+
+:::git init
 git add . -v
-git commit -m "first commit"
+git commit -m "Commit em  %date% às %time%"
 git branch -M main
-git remote add origin git@github.com:Prof-Josimar/crud_php_poo_2026_refatorado.git
 git push -u origin main
+
+::start "" "https://github.com/Prof-Josimar/crud_php_poo_2026"
+
+
+
+
+::git remote add origin git@github.com:Prof-Josimar/crud_php_poo_2026_refatorado.git
